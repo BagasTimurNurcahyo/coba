00bae7eb0add887f4f7ee06e4ec64f02885161ad
BagasTimurNurcahyo <bagastn.22@gmail.com>
2022-10-05 15:43:54 +0700

    update

---
+++ src/components/Login.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import SIgn_img from "./SIgn_img";
+
+const Login = () => {
+  const [inpval, setInpval] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [data, setData] = useState([]);
+  console.log(inpval);
+
+  const getdata = (e) => {
+    const { value, name } = e.target;
+
+    setInpval(() => {
+      return {
+        ...inpval,
+        [name]: value,
+      };
+    });
+  };
+
+  const addData = (e) => {
+    e.preventDefault();
+
+    const getuserArr = localStorage.getItem("userwenotes");
+    console.log(getuserArr);
+
+    const { email, password } = inpval;
+
+    if (email === "") {
+      alert("email field is requred");
+    } else if (!email.includes("@")) {
+      alert("plz enter valid email adderess");
+    } else if (password === "") {
+      alert("password field is requred");
+    } else if (password.length < 5) {
+      alert("password length greater five");
+    } else {
+      if (getuserArr && getuserArr.length) {
+        const userdata = JSON.parse(getuserArr);
+        const userlogin = userdata.filter((el, k) => {
+          return el.email === email && el.password === password;
+        });
+
+        if (userlogin.length === 0) {
+          alert("invalid details");
+        } else {
+          console.log("user login success");
+        }
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="container mt-3">
+        <section className="d-flex justify-content-between">
+          <div className="left_data mt-3 p-3" style={{ width: "100%" }}>
+            <h3 className="text-center col-lg-6">Sign In</h3>
+            <Form>
+              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                <Form.Control
+                  type="email"
+                  name="email"
+                  onChange={getdata}
+                  placeholder="Enter email"
+                />
+              </Form.Group>
+
+              <Form.Group
+                className="mb-3 col-lg-6"
+                controlId="formBasicPassword"
+              >
+                <Form.Control
+                  type="password"
+                  name="password"
+                  onChange={getdata}
+                  placeholder="Password"
+                />
+              </Form.Group>
+              <Button
+                variant="primary"
+                className="col-lg-6"
+                onClick={addData}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Form>
+            <p className="mt-3">
+              Already Have an Account <span>Sign In</span>
+            </p>
+          </div>
+          <SIgn_img />
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Login;
